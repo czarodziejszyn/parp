6040846f0dad782b9eec5bb3269d333f234cfeb5
Szymon <01178585@pw.edu.pl>
2025-04-04 18:52:11 +0200

    poczatek gry

--- prison_escape.pl
+++ prison_escape.pl
@@ -1,10 +1,24 @@
+:- use_module(library(ansi_term)).
+
 lokacja(cela, 'Jesteś w swojej celi.').
 lokacja(stolowka, 'Jesteś na więziennej stołówce.').
 lokacja(spacerniak, 'Jesteś na spacerniaku.').
 lokacja(biblioteka, 'Jesteś w bibliotece.').
-lokacja(korytarz, 'Jesteś na korytarzu.').
+lokacja(pralnia, 'Jesteś w pralni.').
 
-:- dynamic jestem/1.
+:- dynamic pozycja_gracza/1, wykonane/1.
+pozycja_gracza(cela).
+
+idz(Miejsce) :-
+    lokacja(Miejsce, Opis),
+    ansi_format([fg(green)], '~w~n', [Opis]).
+
+idz(_) :-
+    ansi_format([fg(red)], 'Nie ma takiego miejsca!~n', []).
+
+dialog(Postac, Tekst) :-
+    ansi_format([fg(cyan), bold], '~w: ', [Postac]),
+    ansi_format([fg(white)], '~w~n', [Tekst]).
 
 instrukcje :-
     nl,
@@ -14,6 +28,15 @@ instrukcje :-
     write('stop                 -- zakończ rozgrywkę i wyjdź.'), nl,
     nl.
 
+dialog_sad :-
+    nl,
+    dialog('Prokurator', 'Niech Pan opowowie jak z Pana perspektywy wyglądała rozmowa z szefem tego dnia.'),
+    dialog('Andy Dufrasne', 'Był bardzo szorstki. Powiedział, że nikt już nie korzysta z vima, że zmniejsza on moją produktywność i zwolnienie mnie to jego jedyne wyjście.'),
+    dialog('Prokurator', 'Czy po tym incydencie wypchnął Pan na maina gałąź, w której cały kod był pisany w jednej linii, a nazwy funkcji nie oddawały ich działania? Tu przykład: funkcja launch_missle() wypisywała na konsolę "Hello, World".'),
+    dialog('Andy Dufrasne', 'Jak już mówiłem, zostałem wrobiony. Nigdy nie wypchnąłbym kodu przed pokryciu jego przynajmniej dziewięćdziesięciu procent testami jednostkowymi.'),
+    dialog('Sędzia', 'Panie Andrew Dufrasne, za pisanie brzydkiego kodu zostaje Pan skazany na dwa dożywocia w więzieniu o zaostrzonym rygorze!'),
+    nl.
+
 start :-
     instrukcje,
-    rozejrzyj.
+    dialog_sad.
