ab2019ce293d5d9eb5935ec9ed756fad3cfdd376
Szymon <01178585@pw.edu.pl>
2025-05-10 18:55:29 +0200

    stan gry

--- haskell/app/prison_escape.hs
+++ haskell/app/prison_escape.hs
@@ -1,26 +1,117 @@
 import System.Console.ANSI
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.List (intercalate)
+
+
+data Miejsce = Biblioteka | Cela | Pralnia | Spacerniak | Stolowka
+    deriving (Show, Eq, Ord, Enum)
+
+
+data Przedmiot = Atlas | Drut | Farba | Jablko | Klej | Kontakt | Material | Nozyczki | Odkurzacz | Pamietnik | Papier | Pizama | Plaszcze | Sztucce | Sznurek | Srubokret | Ubrania | Wlosy
+    deriving (Show, Eq, Ord)
+
+
+data Postac = Red | Bibliotekarz | Klawisz | Kucharz
+    deriving (Show, Eq, Ord)
+
+
+data StanGry = StanGry
+    { miejsceGracza :: Miejsce
+    , ekwipunek :: [Przedmiot]
+    , przedmiotyLokacji :: Map.Map Miejsce [Przedmiot]
+    , postacieLokacji :: Map.Map Miejsce Postac
+    , dialogi :: Map.Map Postac String
+    , przedmiotyDoUcieczki :: [Przedmiot]
+    }
+
+
+stanPoczatkowy :: StanGry
+stanPoczatkowy = StanGry
+    { miejsceGracza = Cela
+    , ekwipunek = []
+    , przedmiotyLokacji = Map.fromList
+        [ (Biblioteka, [Klej, Papier])
+        , (Cela, [Mydlo, Pamietnik, Pizama])
+        , (Pralnia, [Odkurzacz, Plaszcze, Ubrania])
+        , (Spacerniak, [Farba])
+        , (Stolowka, [Sztucce])
+        ]
+    , postacieLokacji = Map.fromList
+        [ (Biblioteka, Bibliotekarz)
+        , (Pralnia, Klawisz)
+        , (Spacerniak, Red)
+        , (Stolowka, Kucharz)
+        ]
+    , dialogi = Map.fromList
+        [ (Bibliotekarz, "Ciii... Próbuję się skupić na tej książcę o relacyjnych bazach danych.")
+        , (Klawisz, "Nie masz dziś zmiany w pralni? Lepiej się pośpiesz!")
+        , (Kucharz, "To samo co zwykle?")
+        , (Red, "Czyli próbujesz uciec i potrzebujesz kogoś na zewnątrz do pomocy? Znajdź mi w bibliotece atlas, a w zamian zobaczę co da się zrobić.")
+        ]
+    , przedmiotyDoUcieczki = [Drut, Farba, Kontakt, Material, Mydlo, Papier, Plaszcze, Srubokret, Sznurek, Sztucce, Ubrania, Wlosy]
+    }
+
 
 wypiszDialogStartowy :: IO ()
 wypiszDialogStartowy = do
-  let dialogStartowy = 
-        [ ("Prokurator", "Niech Pan opowie jak z Pana perspektywy wyglądała rozmowa z szefem tego dnia.")
-        , ("Andy Dufresne", "Był bardzo szorstki. Powiedział, że nikt już nie korzysta z vima, że zmniejsza on moją produktywność i zwolnienie mnie to jego jedyne wyjście.")
-        , ("Prokurator", "Czy po tym incydencie wypchnął Pan na maina gałąź, w której cały kod był pisany w jednej linii, a nazwy funkcji nie oddawały ich działania? Tu przykład: funkcja launch_missle() wypisywała na konsolę 'Hello, World'.")
-        , ("Andy Dufrasne", "Jak już mówiłem, zostałem wrobiony. Nigdy nie wypchnąłbym kodu przed pokryciu jego przynajmniej dziewięćdziesięciu procent testami jednostkowymi.")
-        , ("Sędzia", "Panie Andrew Dufrasne, za pisanie brzydkiego kodu zostaje Pan skazany na dwa dożywocia w więzieniu o zaostrzonym rygorze!")
-        ]
-  mapM_ (uncurry wypiszDialog) dialogStartowy
+    let dialogStartowy = 
+            [ ("Prokurator", "Niech Pan opowie jak z Pana perspektywy wyglądała rozmowa z szefem tego dnia.")
+            , ("Andy Dufresne", "Był bardzo szorstki. Powiedział, że nikt już nie korzysta z vima, że zmniejsza on moją produktywność i zwolnienie mnie to jego jedyne wyjście.")
+            , ("Prokurator", "Czy po tym incydencie wypchnął Pan na maina gałąź, w której cały kod był pisany w jednej linii, a nazwy funkcji nie oddawały ich działania? Tu przykład: funkcja launch_missle() wypisywała na konsolę 'Hello, World'.")
+            , ("Andy Dufrasne", "Jak już mówiłem, zostałem wrobiony. Nigdy nie wypchnąłbym kodu przed pokryciu jego przynajmniej dziewięćdziesięciu procent testami jednostkowymi.")
+            , ("Sędzia", "Panie Andrew Dufrasne, za pisanie brzydkiego kodu zostaje Pan skazany na dwa dożywocia w więzieniu o zaostrzonym rygorze!")
+            ]
+    mapM_ (uncurry wypiszDialog) dialogStartowy
+
 
 wypiszDialog :: String -> String -> IO ()
 wypiszDialog postac tekst = do
-  setSGR [SetColor Foreground Vivid Cyan]
-  putStr postac
-  putStr ": "
-  setSGR [SetColor Foreground Vivid White]
-  putStrLn tekst
-  setSGR [Reset]
+    setSGR [SetColor Foreground Vivid Cyan]
+    putStr postac
+    putStr ": "
+    setSGR [SetColor Foreground Vivid White]
+    putStrLn tekst
+    setSGR [Reset]
+
+
+wypisz :: [String] -> IO ()
+wypisz xs = putStr (unlines xs)
+
+
+instrukcje :: [String]
+instrukcje = [
+    ""
+    , "Dostępne komendy:"
+    , "ekwipunek                -- sprawdź czy masz już wszystko do ucieczki."
+    , ""
+    ]
+
+
+wypiszInstrukcje :: IO ()
+wypiszInstrukcje = wypisz instrukcje
+
+
+wczytajKomende :: IO String
+wczytajKomende = do
+    putStr "> "
+    xs <- getLine
+    return xs
+
+
+petlaGry :: IO ()
+petlaGry = do
+    cmd <- wczytajKomende
+    case cmd of
+        "instrukcje" -> do wypiszInstrukcje
+                           petlaGry
+        "quit" -> return ()
+        _ -> do wypisz ["Nieznana komenda.", ""]
+                petlaGry
+
 
 main :: IO ()
 main = do
-  wypiszDialogStartowy
-
+    wypiszDialogStartowy
+    wypiszInstrukcje
+    petlaGry
--- haskell/dist-newstyle/build/x86_64-linux/ghc-9.6.7/haskell-0.1.0.0/x/haskell/build/haskell/autogen/cabal_macros.h
+++ haskell/dist-newstyle/build/x86_64-linux/ghc-9.6.7/haskell-0.1.0.0/x/haskell/build/haskell/autogen/cabal_macros.h
@@ -30,6 +30,26 @@
   (major1) == 4 && (major2) <  18 || \
   (major1) == 4 && (major2) == 18 && (minor) <= 3)
 #endif /* MIN_VERSION_base */
+/* package containers-0.6.7 */
+#ifndef VERSION_containers
+#define VERSION_containers "0.6.7"
+#endif /* VERSION_containers */
+#ifndef MIN_VERSION_containers
+#define MIN_VERSION_containers(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  6 || \
+  (major1) == 0 && (major2) == 6 && (minor) <= 7)
+#endif /* MIN_VERSION_containers */
+/* package mtl-2.3.1 */
+#ifndef VERSION_mtl
+#define VERSION_mtl "2.3.1"
+#endif /* VERSION_mtl */
+#ifndef MIN_VERSION_mtl
+#define MIN_VERSION_mtl(major1,major2,minor) (\
+  (major1) <  2 || \
+  (major1) == 2 && (major2) <  3 || \
+  (major1) == 2 && (major2) == 3 && (minor) <= 1)
+#endif /* MIN_VERSION_mtl */
 
 /* tool gcc-15.1.1 */
 #ifndef TOOL_VERSION_gcc
