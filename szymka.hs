25427df6bd7f01e4e2136e2fd7575a74397f7f61
krzy-sok <01178595@pw.edu.pl>
2025-05-11 19:52:57 +0200

    removed commented out functions

--- szymka.hs
+++ szymka.hs
@@ -761,21 +761,6 @@ czesc2 = do
 
 -- czesc 3!
 
-
--- common --
-
-
--- -- print --
--- printBlue :: [String] -> IO ()
--- printBlue text = putStr $ "\x1b[34m" ++ unlines text ++ "\x1b[0m"
--- printRed :: [String] -> IO ()
--- printRed text = putStr $ "\x1b[31m" ++ unlines text ++ "\x1b[0m"
--- printYellow :: [String] -> IO ()
--- printYellow text = putStr $ "\x1b[33m" ++ unlines text ++ "\x1b[0m"
--- printGreen :: [String] -> IO ()
--- printGreen text = putStr $ "\x1b[32m" ++ unlines text ++ "\x1b[0m"
-
-
 -- paths
 
 paths :: Map.Map (String, String) String
